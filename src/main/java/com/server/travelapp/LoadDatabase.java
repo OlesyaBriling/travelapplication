9d9a1d470df3c038cb4bbc690dd10abcb81595c1
Olesya Briling <olesabrilng@gmail.com>
2021-03-22 16:34:46 +0300

    v2

--- src/main/java/com/server/travelapp/LoadDatabase.java
+++ src/main/java/com/server/travelapp/LoadDatabase.java
@@ -1,23 +1,24 @@
 package com.server.travelapp;
 
+
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.logging.Logger;
 
 @Configuration
 public class LoadDatabase {
 
-    private static final Logger log =  Logger.getLogger(LoadDatabase.class);
+    private static final Logger log =  LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
     CommandLineRunner initDatabase(PlacesRepository repository) {
 
         return args -> {
-            log.info("Preloading " = repository.save(new Places("Петергоф")));
-
+            log.info("Preloading " + repository.save(new Places("Петергоф")));
+            log.info("Preloading " + repository.save(new Places("Мост")));
         };
     }
 
--- src/main/java/com/server/travelapp/PlacesRepository.java
+++ src/main/java/com/server/travelapp/PlacesRepository.java
@@ -2,8 +2,7 @@ package com.server.travelapp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 
 interface PlacesRepository extends JpaRepository<Places, Long> {
-    Collection<Places> findPlacesName(String placeName);
+
 }
